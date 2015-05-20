97807d87356f11c49c0b810c883d166df1d643b4
PJ Dietz <pjdietz@gmail.com>
2015-05-20 19:23:43 -0400

    Remove RTD custom theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,7 +70,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['theme']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -194,5 +194,3 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-html_style = 'css/custom-style.css'
