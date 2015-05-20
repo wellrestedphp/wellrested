217944643347c83128a48e3ce5b8371b57deb534
PJ Dietz <pjdietz@gmail.com>
2015-05-20 19:04:16 -0400

    Fixing Read the Docs custom theming

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,7 +70,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['theme']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -195,4 +195,4 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_style = 'css/style.css'
+html_style = 'css/custom-style.css'
