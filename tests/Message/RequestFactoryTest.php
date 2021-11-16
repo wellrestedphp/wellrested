e77bf3979e1db785ad11035091bf04b7149ae1e3
Joe Ginley <joeginley@gintechsystems.com>
2021-11-16 11:10:07 -0500

    Updated request factory unit tests.

--- tests/Message/RequestFactoryTest.php
+++ tests/Message/RequestFactoryTest.php
@@ -6,19 +6,7 @@ use WellRESTed\Test\TestCase;
 
 class RequestFactoryTest extends TestCase
 {
-    public function testCreatesRequestDELETE(): void
-    {
-        $method = 'DELETE';
-        $uri = 'http://localhost:8080';
-
-        $factory = new RequestFactory();
-        $request = $factory->createRequest($method, $uri);
-
-        $this->assertEquals($method, $request->getMethod());
-        $this->assertEquals($uri, $request->getUri());
-    }
-
-    public function testCreatesRequestGET(): void
+    public function testCreatesRequestFromString(): void
     {
         $method = 'GET';
         $uri = 'http://localhost:8080';
@@ -30,10 +18,10 @@ class RequestFactoryTest extends TestCase
         $this->assertEquals($uri, $request->getUri());
     }
 
-    public function testCreatesRequestPOST(): void
+    public function testCreatesRequestFromUri(): void
     {
         $method = 'POST';
-        $uri = 'http://localhost:8080';
+        $uri = new Uri('http://localhost:8080');
 
         $factory = new RequestFactory();
         $request = $factory->createRequest($method, $uri);
